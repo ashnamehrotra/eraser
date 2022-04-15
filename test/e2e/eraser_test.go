0dff933b485cb29a5e052a97264fd8d4da687860
ashnamehrotra <ashnamehrotra@gmail.com>
2022-04-15 09:58:25 -0400

    check for removed image after deployment

--- test/e2e/eraser_test.go
+++ test/e2e/eraser_test.go
@@ -223,13 +223,17 @@ func TestRemoveImagesFromAllNodes(t *testing.T) {
 				t.Error("Failed to deploy image list config", err)
 			}
 
+			ctxT, cancel := context.WithTimeout(ctx, 5*time.Minute)
+			defer cancel()
+			checkImageRemoved(ctxT, t, getClusterNodes(t), nginx)
+
 			if err := deployEraserConfig(cfg.KubeconfigFile(), "eraser-system", "test-data", "eraser_v1alpha1_imagelist_updated.yaml"); err != nil {
 				t.Error("Failed to deploy image list config", err)
 			}
 
-			ctxT, cancel := context.WithTimeout(ctx, 5*time.Minute)
-			defer cancel()
-			checkImageRemoved(ctxT, t, getClusterNodes(t), nginx)
+			/*	ctxT, cancel := context.WithTimeout(ctx, 5*time.Minute)
+				defer cancel()
+				checkImageRemoved(ctxT, t, getClusterNodes(t), nginx) */
 
 			/*	ctxT, cancel = context.WithTimeout(ctx, 5*time.Minute)
 				defer cancel()
