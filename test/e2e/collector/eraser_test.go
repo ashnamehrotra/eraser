cb23dee94e9b1ac4ceeeee9819a3f0f6ab42aacf
ashnamehrotra <ashnamehrotra@gmail.com>
2022-06-24 19:59:35 -0400

    test excluded with collector e2e

--- test/e2e/collector/eraser_test.go
+++ test/e2e/collector/eraser_test.go
@@ -10,7 +10,7 @@ import (
 
 	eraserv1alpha1 "github.com/Azure/eraser/api/v1alpha1"
 	"github.com/Azure/eraser/test/e2e/util"
-	appsv1 "k8s.io/api/apps/v1"
+	//appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
@@ -30,7 +30,7 @@ func TestRemoveImagesFromAllNodes(t *testing.T) {
 		nginx  = "nginx"
 	)
 
-	collectScanErasePipelineFeat := features.New("Test Remove Image From All Nodes").
+	/*collectScanErasePipelineFeat := features.New("Test Remove Image From All Nodes").
 		Setup(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 			wd, err := os.Getwd()
 			if err != nil {
@@ -270,29 +270,22 @@ func TestRemoveImagesFromAllNodes(t *testing.T) {
 
 			return ctx
 		}).
-		Feature()
+		Feature()*/
 
 	excludedFeat := features.New("Test Excluded").
 		Setup(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 			wd, err := os.Getwd()
 			if err != nil {
-				t.Error("Could not get working directory", err)
+				t.Error("Could not get wd")
 			}
 
 			providerResourceAbsolutePath, err := filepath.Abs(filepath.Join(wd, "/../../../", providerResourceDirectory, "eraser"))
 			if err != nil {
-				t.Error("Unable to get provider resource absolute path", err)
+				t.Error("Could not get provider resource absolute pathy")
 			}
-
-			err = util.HelmInstall(cfg.KubeconfigFile(), "eraser-system", []string{providerResourceAbsolutePath, "--set", "collector.image.repository="})
-			if err != nil {
-				t.Error("Unable to install deployment with no collector disabled", err)
-			}
-
-			podSelectorLabels := map[string]string{"app": nginx}
-			nginxDep := util.NewDeployment(cfg.Namespace(), nginx, 2, podSelectorLabels, corev1.Container{Image: nginx, Name: nginx})
-			if err := cfg.Client().Resources().Create(ctx, nginxDep); err != nil {
-				t.Error("Failed to create the deployment", err)
+			// start deployment
+			if err := util.HelmInstall(cfg.KubeconfigFile(), "eraser-system", []string{providerResourceAbsolutePath}); err != nil {
+				t.Error("Unable to helm install deployment", err)
 			}
 
 			// create excluded configmap and add docker.io/library/*
@@ -309,71 +302,70 @@ func TestRemoveImagesFromAllNodes(t *testing.T) {
 
 			return ctx
 		}).
-		Assess("deployment successfully deployed", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			client, err := cfg.NewClient()
+		Assess("ImageCollector CR is generated", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			c, err := cfg.NewClient()
 			if err != nil {
-				t.Error("Failed to create new client", err)
+				t.Fatal("Failed to create new client", err)
 			}
 
-			resultDeployment := appsv1.Deployment{
-				ObjectMeta: metav1.ObjectMeta{Name: nginx, Namespace: cfg.Namespace()},
-			}
-
-			if err = wait.For(conditions.New(client.Resources()).DeploymentConditionMatch(&resultDeployment, appsv1.DeploymentAvailable, corev1.ConditionTrue),
-				wait.WithTimeout(time.Minute*3)); err != nil {
-				t.Error("deployment not found", err)
-			}
-
-			return context.WithValue(ctx, nginx, &resultDeployment)
-		}).
-		Assess("Check image remains in all nodes", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			// delete deployment
-			client, err := cfg.NewClient()
-			if err != nil {
-				t.Error("Failed to create new client", err)
-			}
-
-			var pods corev1.PodList
-			err = client.Resources().List(ctx, &pods, func(o *metav1.ListOptions) {
-				o.LabelSelector = labels.SelectorFromSet(labels.Set{"app": nginx}).String()
-			})
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			dep := ctx.Value(nginx).(*appsv1.Deployment)
-			if err := client.Resources().Delete(ctx, dep); err != nil {
-				t.Error("Failed to delete the dep", err)
-			}
-
-			for _, nodeName := range util.GetClusterNodes(t) {
-				err := wait.For(util.ContainerNotPresentOnNode(nodeName, nginx), wait.WithTimeout(time.Minute*2))
+			resource := eraserv1alpha1.ImageCollector{}
+			wait.For(func() (bool, error) {
+				err := c.Resources().Get(ctx, "imagecollector-shared", "default", &resource)
 				if err != nil {
-					t.Logf("error while waiting for deployment deletion: %v", err)
+					return false, err
 				}
+
+				if resource.ObjectMeta.Name == "imagecollector-shared" {
+					return true, nil
+				}
+
+				return false, nil
+			}, wait.WithTimeout(time.Minute*3))
+
+			return ctx
+		}).
+		Assess("ImageList CR is generated", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			c, err := cfg.NewClient()
+			if err != nil {
+				t.Fatal("Failed to create new client", err)
 			}
 
-			// create imagelist to trigger deletion
-			if err := util.DeployEraserConfig(cfg.KubeconfigFile(), "eraser-system", "../test-data", "eraser_v1alpha1_imagelist.yaml"); err != nil {
-				t.Error("Failed to deploy image list config", err)
-			}
+			resource := eraserv1alpha1.ImageList{}
+			wait.For(func() (bool, error) {
+				err := c.Resources().Get(ctx, "imagelist", "default", &resource)
+				if util.IsNotFound(err) {
+					return false, nil
+				}
 
-			ctxT, cancel := context.WithTimeout(ctx, time.Minute)
+				if err != nil {
+					return false, err
+				}
+
+				if resource.ObjectMeta.Name == "imagelist" {
+					return true, nil
+				}
+
+				return false, nil
+			}, wait.WithTimeout(time.Minute*3))
+
+			return ctx
+		}).
+		Assess("Images successfully not deleted from all nodes", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			ctxT, cancel := context.WithTimeout(ctx, 3*time.Minute)
 			defer cancel()
-			// since docker.io/library/* was excluded, nginx should still exist following deletion
-			util.CheckImagesExist(ctxT, t, util.GetClusterNodes(t), nginx)
+			util.CheckImagesExist(ctxT, t, util.GetClusterNodes(t), alpine)
 
 			return ctx
 		}).
 		Assess("Pods from imagejobs are cleaned up", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 			c, err := cfg.NewClient()
 			if err != nil {
-				t.Error("Failed to create new client", err)
+				t.Fatal("Failed to create new client", err)
 			}
 
 			var ls corev1.PodList
 			err = c.Resources().List(ctx, &ls, func(o *metav1.ListOptions) {
-				o.LabelSelector = labels.SelectorFromSet(map[string]string{"name": "eraser"}).String()
+				o.LabelSelector = labels.SelectorFromSet(map[string]string{"name": "collector"}).String()
 			})
 			if err != nil {
 				t.Errorf("could not list pods: %v", err)
@@ -399,7 +391,7 @@ func TestRemoveImagesFromAllNodes(t *testing.T) {
 
 			var ls corev1.PodList
 			err = c.Resources().List(ctx, &ls, func(o *metav1.ListOptions) {
-				o.LabelSelector = labels.SelectorFromSet(map[string]string{"name": "eraser-controller"}).String()
+				o.LabelSelector = labels.SelectorFromSet(map[string]string{"name": "eraser-manager"}).String()
 			})
 			if err != nil {
 				t.Error("could not list eraser manager pod")
@@ -409,11 +401,12 @@ func TestRemoveImagesFromAllNodes(t *testing.T) {
 			if err != nil {
 				t.Errorf("error waiting for eraser-manager to be deleted: %v", err)
 			}
+
 			return ctx
 		}).
 		Feature()
 
-	testenv.Test(t, disableScanFeat)
-	testenv.Test(t, collectScanErasePipelineFeat)
+	//testenv.Test(t, disableScanFeat)
+	//testenv.Test(t, collectScanErasePipelineFeat)
 	testenv.Test(t, excludedFeat)
 }
