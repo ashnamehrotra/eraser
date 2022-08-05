fb20907524e9c04862d8e5136e71a2edea167de4
ashnamehrotra <ashnamehrotra@gmail.com>
2022-08-05 09:36:41 -0400

    overwrite trivy logger to disable scan logs

--- pkg/scanners/trivy/trivy.go
+++ pkg/scanners/trivy/trivy.go
@@ -18,6 +18,7 @@ import (
 	"github.com/aquasecurity/fanal/artifact"
 	artifactImage "github.com/aquasecurity/fanal/artifact/image"
 	fanalImage "github.com/aquasecurity/fanal/image"
+	trivylogger "github.com/aquasecurity/trivy/pkg/log"
 	"github.com/aquasecurity/trivy/pkg/scanner"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
@@ -91,9 +92,15 @@ func main() {
 	flag.Parse()
 	ctx := context.Background()
 
-	if err := logger.Configure(); err != nil {
+	var err error
+
+	if err = logger.Configure(); err != nil {
 		fmt.Fprintln(os.Stderr, "Error setting up logger:", err)
 		os.Exit(generalErr)
+	}
+
+	if trivylogger.Logger, err = trivylogger.NewLogger(false, true); err != nil {
+		log.Error(err, "Unable to disable trivy logger")
 	}
 
 	if *enableProfile {
