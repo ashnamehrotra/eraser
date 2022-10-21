682f809f3653a72d1d39ffe1e7d3dbb66c050bc8
ashnamehrotra <ashnamehrotra@gmail.com>
2022-10-21 13:33:55 -0700

    test without deployOtelCollector

--- test/e2e/tests/metrics_test/main_test.go
+++ test/e2e/tests/metrics_test/main_test.go
@@ -27,7 +27,7 @@ func TestMain(m *testing.M) {
 	util.Testenv.Setup(
 		envfuncs.CreateKindClusterWithConfig(util.KindClusterName, util.NodeVersion, "../../kind-config.yaml"),
 		envfuncs.CreateNamespace(util.TestNamespace),
-		util.DeployOtelCollector(util.TestNamespace),
+		//util.DeployOtelCollector(util.TestNamespace),
 		envfuncs.LoadDockerImageToCluster(util.KindClusterName, util.ManagerImage),
 		envfuncs.LoadDockerImageToCluster(util.KindClusterName, util.Image),
 		envfuncs.LoadDockerImageToCluster(util.KindClusterName, util.VulnerableImage),
